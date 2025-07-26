89a4c20ec3d9a6d194e0a71e43ae542aa2140c1e
Jan Luebbe <jlu@pengutronix.de>
2025-07-26 16:42:55 +0200

    gluon-radv-filterd: down override statistics.gateway6
    Nodes which are a batman gateway route traffic themselves instead of relying on the
    batman-selected gateway. Another respondd plugin sets that value, so we
    shouldn't replace it.

--- package/gluon-radv-filterd/src/respondd.c
+++ package/gluon-radv-filterd/src/respondd.c
@@ -4,8 +4,60 @@
 #include <libgluonutil.h>
 #include <net/ethernet.h>
 #include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+#include <netlink/netlink.h>
+#include <netlink/genl/genl.h>
+#include <netlink/genl/ctrl.h>
+#include <batadv-genl.h>
 
 #include "mac.h"
+
+struct mode_netlink_opts {
+	bool is_server;
+	struct batadv_nlquery_opts query_opts;
+};
+
+static const enum batadv_nl_attrs mode_mandatory[] = {
+	BATADV_ATTR_GW_MODE,
+};
+
+static int parse_mode_netlink_cb(struct nl_msg *msg, void *arg)
+{
+	struct nlattr *attrs[BATADV_ATTR_MAX+1];
+	struct nlmsghdr *nlh = nlmsg_hdr(msg);
+	struct batadv_nlquery_opts *query_opts = arg;
+	struct genlmsghdr *ghdr;
+	struct mode_netlink_opts *opts;
+	uint8_t mode;
+
+	opts = batadv_container_of(query_opts, struct mode_netlink_opts,
+				   query_opts);
+
+	if (!genlmsg_valid_hdr(nlh, 0))
+		return NL_OK;
+
+	ghdr = nlmsg_data(nlh);
+
+	if (ghdr->cmd != BATADV_CMD_GET_MESH_INFO)
+		return NL_OK;
+
+	if (nla_parse(attrs, BATADV_ATTR_MAX, genlmsg_attrdata(ghdr, 0),
+	    genlmsg_len(ghdr), batadv_genl_policy))
+		return NL_OK;
+
+	if (batadv_genl_missing_attrs(attrs, mode_mandatory,
+	    BATADV_ARRAY_SIZE(mode_mandatory)))
+		return NL_OK;
+
+	mode = nla_get_u8(attrs[BATADV_ATTR_GW_MODE]);
+
+	opts->is_server = mode == BATADV_GW_MODE_SERVER;
+
+	return NL_OK;
+}
 
 static struct json_object * get_radv_filter() {
 	FILE *f = popen("exec ebtables-tiny -L RADV_FILTER", "r");
@@ -37,6 +89,22 @@ static struct json_object * get_radv_filter() {
 
 static struct json_object * respondd_provider_statistics() {
 	struct json_object *ret = json_object_new_object();
+
+	struct mode_netlink_opts opts = {
+                .is_server = false,
+                .query_opts = {
+                        .err = 0,
+                },
+        };
+
+        batadv_genl_query("bat0", BATADV_CMD_GET_MESH_INFO,
+                          parse_mode_netlink_cb, 0,
+                          &opts.query_opts);
+
+	if (opts.is_server) {
+		// We are a batman gateway, do not write gateway6
+		return ret;
+	}
 
 	json_object_object_add(ret, "gateway6", get_radv_filter());
 
